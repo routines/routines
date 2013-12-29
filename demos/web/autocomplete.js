5dba3c29eeff3f8d0c4b93ee4c866be148170411
Joubert Nel <joubert@joubster.com>
2013-12-29 08:13:04 -0800

    aligns code with the code walkthru at async-es.github.com

--- demos/web/autocomplete.js
+++ demos/web/autocomplete.js
@@ -1,4 +1,30 @@
+if (!$async) {
+    // It seems that traceur *sometimes*, fails to properly insert our
+    // code from async-es.js
+    // When that happens, we simply ask the user to reload the page.
+    alert('An unexpected error occurred. Please reload the page.');
+}
+
 (function(Channel, go, listen, unique, pace, jsonp) {
+
+    var input = document.getElementById('searchtext'),
+        results = document.getElementById('results');
+
+    function clearResults() {
+        results.innerHTML = '';
+    }
+
+    function showResults(terms) {
+        var i, len, p;
+
+        clearResults();
+        
+        for (i = 0, len = terms.length; i < len; i++) {
+            p = document.createElement('p');
+            p.innerHTML = terms[i];
+            results.appendChild(p);
+        }                
+    }
 
     function searchWikipedia(term) {
         var url = 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?&search=' +
@@ -6,40 +32,38 @@
         return jsonp(url);
     }
 
+
+    function showError(err) {
+        results.innerHTML = '<h1p>Error:</h1>' + err;
+    }
+
     go(function* () {
-        var input = document.getElementById('searchtext'),
-            results = document.getElementById('results'),
-            keyup = unique(listen(input, 'keyup')),
-            pacedKeyup = pace(keyup, 750),
-            evt,
-            data,
-            res,
-            i,
-            len,
-            p,
-            text;
+        var keyup = listen(input, 'keyup'),
+            pacedKeyup = pace(keyup, 300),
+            evt, data, res, text, previousText,
+            minLength;
         
         while (true) {
             evt = yield pacedKeyup.get();
             text = evt.target.value;
-            
-            if (text.length > 2) {
+            minLength = text.length > 2;
+
+            if (minLength && text !== previousText) {                
                 data = yield searchWikipedia(text).get();
                 
                 if (data.error) {
-                    results.innerHTML = '<h1p>Error:</h1>' + JSON.stringify(data.error);
+                    showError(JSON.stringify(data.error));
                 } else {                    
                     if (data[0] && data[1]) {
-                        results.innerHTML = '';
-                        res = data[1];
-                        for (i = 0, len = res.length; i < len; i++) {
-                            p = document.createElement('p');
-                            p.innerHTML = res[i];
-                            results.appendChild(p);
-                        }                
+                        showResults(data[1]);
                     }
                 }
+                
+            } else if (!minLength) {
+                clearResults();
             }
+
+            previousText = text;
         }
     });
 
