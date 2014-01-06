aecd4bf53d86f0b9a997da48b9ac465e8dbfa9b3
Primeagen <michael.b.paulson@gmail.com>
2014-01-05 20:08:46 -0800

    Saving the server stuff

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var path = require('path');
 var app = express();
 
 app
-    .use('/', express.static(path.join(__dirname, '/demos')))
-    .use('/', express.directory(path.join(__dirname, '/demos')))
+    .use('/', express.static(path.join(__dirname, '/')))
+    .use('/', express.directory(path.join(__dirname, '/')))
     .listen('3000');
