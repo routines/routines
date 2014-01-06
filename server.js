90182e0460a1a34782485d919f8894c764c74daf
Joubert Nel <joubert@joubster.com>
2014-01-05 21:11:27 -0800

    removes server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-
-app
-    .use('/', express.static(path.join(__dirname, '/')))
-    .use('/', express.directory(path.join(__dirname, '/')))
-    .listen('3000');
