d01d014bbcf0c2a2bb9cdf216536ae883066c6c9
Joubert Nel <joubert@joubster.com>
2014-01-14 20:09:57 -0800

    bumps to v0.1.2

--- demos/node/es6/files/main.js
+++ demos/node/es6/files/main.js
@@ -2,21 +2,8 @@ var fs = require('fs'),
     jspipe = require('jspipe'),
     Pipe = jspipe.Pipe,
     job = jspipe.job,
+    denode = jspipe.denode,
     filenames = ['file1.txt', 'file2.txt', 'file3.txt'];
-
-function denode(fn, args) {
-    var pipe = new Pipe(),
-        newArgs = args instanceof Array ? args : [args];
-
-    newArgs.push(function(err, data) {
-        var result = err ? {err:err} : {data:data};
-        pipe.send(result);
-    });
-    
-    fn.apply(fn, newArgs);
-    return pipe;
-}
-
 
 job(function* () {
     var msg,
